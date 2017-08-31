868e944cef8513e4b09db16c201f3cd769fed9cb
zhangling <zl2@winbaoxian.com>
2017-08-31 22:41:09 +0800

    播放器组件（1）通过actions修改数据|基础样式

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,3 +2,12 @@
  * 异步操作
  * mutation做一些封装（多个mutation时）
  */
+import * as types from './mutation-types'
+
+export const selectPlay = function({commit, state}, {list, index}) {
+  commit(types.SET_SEQUENCE_LIST, list)
+  commit(types.SET_PLAYLIST, list)
+  commit(types.SET_CURRENT_INDEX, index)
+  commit(types.SET_FULL_SCREEN, true)
+  commit(types.SET_PLAYING_STATE, true)
+}
