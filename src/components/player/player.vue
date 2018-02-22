9edb02ccf67ae1ebaee493732fd5b08501049c94
zhangling <zl2@winbaoxian.com>
2018-02-22 15:36:53 +0800

    进度条控件(1)进度显示

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -28,7 +28,9 @@
         <div class="bottom">
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
-            <div class="progress-bar-wrapper"></div>
+            <div class="progress-bar-wrapper">
+              <progress-bar :percent="percent"></progress-bar>
+            </div>
             <span class="time time-r">{{format(currentSong.duration)}}</span>
           </div>
           <div class="operators">
@@ -90,10 +92,14 @@
   import {ERR_OK} from 'api/config'
   import animations from 'create-keyframe-animation'
   import {prefixStyle} from 'common/js/dom'
+  import ProgressBar from 'base/progress-bar/progress-bar'
 
   const transform = prefixStyle('transform')
 
   export default {
+    components: {
+      ProgressBar
+    },
     data() {
       return {
         currentSongUrl: '',
@@ -113,6 +119,9 @@
       },
       disableCls() {
         return this.songReady ? '' : 'disable'
+      },
+      percent() {
+        return this.currentTime / this.currentSong.duration
       },
       ...mapGetters([
         'fullScreen',
